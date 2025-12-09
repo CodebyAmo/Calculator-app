59e25bb1662d5e397307109640a80d85d0560d32
CodebyAmo <amo.amogelang023@gmail.com>
2025-12-09 02:10:34 +0200

    A simple calculator built using Html ,CSS and JavaScript

---
+++ script.js
@@ -0,0 +1,59 @@
+            //boolean variable .determines if the next thing the user types should be on a new line.
+            var newLine=true; 
+            var  value1;
+            var currentOperator;
+
+            //Event handler for when any digit button is pressed
+            function digitBtnPressed(button){
+
+                if(newLine){
+                    document.getElementById("inputBox").value =button;
+                    newLine=false;
+                }
+                else{
+                    var currentValue=document.getElementById("inputBox").value;
+                    document.getElementById("inputBox").value=currentValue+button;
+                }
+                
+            }
+
+            //Event handler when ac button is pressed
+            function btnACPressed(){
+                 document.getElementById("inputBox").value =0;
+                 newLine=true;
+            }
+
+            //Event handler for operator buttons 
+            function operatorBtnPressed(operator){
+                currentOperator=operator;
+                value1=parseInt(document.getElementById("inputBox").value);
+                newLine=true;
+            }
+
+            //Event handler for equals to button
+            function equalsBtnPressed(){
+                var value2=parseInt(document.getElementById("inputBox").value);
+                var finalTotal;
+
+                switch(currentOperator){
+                    case"+":
+                    finalTotal=value1+value2;
+                    break;
+
+                      case"-":
+                    finalTotal=value1-value2;
+                    break;
+
+                    case"/":
+                    finalTotal=value1/value2;
+                    break;
+
+                     case"x":
+                    finalTotal=value1*value2;
+                    break;
+                    
+                }
+                document.getElementById("inputBox").value=finalTotal;
+                value1=0;
+                newLine=true;
+            }
